3708ff88b9d79b56f02a953d7ea1398ce2d91798
unknown <zafarhusanovdev@gmail.com>
2025-11-13 12:21:35 +0500

    SEO meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// // @ts-check
+// import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+
+
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://astro-mauve-six.vercel.app/',
+  integrations: [tailwind(), sitemap(), compress()],
+  build: {
+    inlineStylesheets: 'auto',
+  },
+});
+
